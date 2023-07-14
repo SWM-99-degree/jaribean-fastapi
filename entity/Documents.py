7a807faa49612f9b96f3fedc32a3f1d788cea5ba
kisung <chlrltjd5263@naver.com>
2023-07-14 16:24:24 +0900

    feat : <기성 #1> Entity(Model) 추가

--- entity/Documents.py
+++ entity/Documents.py
@@ -1,7 +1,37 @@
-from pydantic import BaseModel, PyObject
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from pydantic import BaseModel, Field
+from datetime import datetime
+from geojson import Point
+from enum.Enum import UserRole
+
+class Matching(BaseModel):
+    id : str = Field(alias = "_id")
+    userId : str
+    cafeId : str
+    number : int
+    matchingTime : datetime
+
 
 class User(BaseModel):
-    id : PyObject
+    id : str = Field(alias = "_id")
+    userName : str
+    userNickname : str
+    userRole : UserRole
+    createdAt : datetime
+    modifiedAt : datetime
+    
 
 
 class Cafe(BaseModel):
+    id : str = Field(alias = "_id")
+    cafeName : str
+    cafePhoneNumber : str
+    cafeAddress : str
+    cafeImg : str
+    coordinate : Point
+    userRole : UserRole
+    createdAt : datetime
+    modifiedAt : datetime
+
